ca3a7f5aca185e783d41131385d10d1d31b66f9f
littleflow3r <36025078+littleflow3r@users.noreply.github.com>
2024-04-29 16:10:48 +0200

    Update run_icl.sh

--- run_icl.sh
+++ run_icl.sh
@@ -6,7 +6,7 @@ mtask='semeval'
 for k in "16-1" "16-4" "16-5" "16-6" "16-9"; do
     input_path="datasets/${mtask}/"
     checkpoint_path="checkpoints/${mtask}/"
-    python src/fewshots.py \
+    python src/icl.py \
     --input_path ${input_path} \
     --checkpoint_path ${checkpoint_path} \
     --model ${model} \
